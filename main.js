95ad945d7ce35daf81417035cf6d27f0f7a6b992
Bahan-RDP <120382980+Bahan-RDP@users.noreply.github.com>
2022-12-19 05:15:26 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -204,7 +204,7 @@ fs.writeFileSync('./options/sticker/welcome.jpg', buffer)
 let ppnya = await TelegraPh('./options/sticker/welcome.jpg')
 let namenya = await ronzz.getName(num.split('@')[0]+"@s.whatsapp.net")
 let welcomenya = `https://api.popcat.xyz/welcomecard?background=https://cdn.discordapp.com/attachments/850808002545319957/859359637106065408/bg.png&text1=${namenya}&text2=Welcome+To+${metadata.subject}&text3=Member+${metadata.participants.length ? metadata.participants.length : "Undefined"}&avatar=${ppnya}`
-const bio = (await ronzz.fetchStatus(num).catch(console.error) || {}).status || '-'
+const bio = await ronzz.fetchStatus(num).status || '-'
 await ronzz.sendMessage(
 update.id, 
 { 
@@ -237,7 +237,7 @@ fs.writeFileSync('./options/sticker/left.jpg', buffer)
 let ppnya = await TelegraPh('./options/sticker/left.jpg')
 let namenya = await ronzz.getName(num.split('@')[0]+"@s.whatsapp.net")
 let leftnya = `https://api.popcat.xyz/welcomecard?background=https://cdn.discordapp.com/attachments/850808002545319957/859359637106065408/bg.png&text1=${namenya}&text2=Leave+From+${metadata.subject}&text3=Member+${metadata.participants.length ? metadata.participants.length : "Undefined"}&avatar=${ppnya}`
-const bio = (await ronzz.fetchStatus(num).catch(console.error) || {}).status || '-'
+const bio = await ronzz.fetchStatus(num).status || '-'
 await ronzz.sendMessage(
 update.id, 
 {
