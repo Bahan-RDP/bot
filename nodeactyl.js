0f364da1f8c6d6e664bb1b5fe55fe44a6b6b5a84
Bahan-RDP <120382980+Bahan-RDP@users.noreply.github.com>
2023-01-02 15:51:41 +0700

    Add files via upload

--- nodeactyl.js
+++ nodeactyl.js
@@ -6,6 +6,21 @@ const Application = new Nodeactyl.NodeactylApplication(
   host,
   application.api_key
 );
+const eggDetails(nest, egg) {
+    const y = await axios({
+      url: `${host}/api/application/nests/${nest}/eggs/${egg}`,
+      method: "GET",
+      followRedirect: true,
+      maxRedirects: 5,
+      headers: {
+        Authorization: "Bearer " + application.api_key,
+        "Content-Type": "application/json",
+        Accept: "Application/vnd.pterodactyl.v1+json",
+      }
+    });
+    return y.data;
+  }
 
-exports.application = Application;
+exports.eggDetails = eggDetails
+exports.Application = Application;
 exports.Nodeactyl = Nodeactyl;
